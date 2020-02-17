2361ca2cd3e795e21b2c43d70f2db89f3305179f
msoliman5 <54380486+msoliman5@users.noreply.github.com>
2020-02-16 16:08:21 -0800

    Add files via upload

---
+++ College.h
@@ -0,0 +1,32 @@
+#ifndef COLLEGE_H
+#define COLLEGE_H
+
+#include <QObject>
+#include <QString>
+
+class College : public QObject
+{
+    Q_OBJECT
+public:
+    explicit College(QObject *parent = nullptr);
+
+
+    QString startcollegeName() const;
+    void setstartCollegeName(const QString &item);
+
+    QString distance() const;
+    void setDistance(QString distance);
+
+    QString endcollegeName() const;
+    void setendCollegeName(const QString &item);
+
+signals:
+
+public slots:
+private:
+    QString startCollegeName;
+    QString Distance;
+    QString endCollegeName;
+};
+
+#endif // COLLEGE_H
